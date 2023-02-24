78a9c4c47713320bf4a63f8b6a78669d96f1776a
Dmitry-Lipinskiy <Dimalipa@mail.ru>
2023-02-24 20:38:10 +0300

    added methods isRise and isIncrease to the employees-list-item

--- employees/src/components/employees-list-item/employees-list-item.jsx
+++ employees/src/components/employees-list-item/employees-list-item.jsx
@@ -1,24 +1,69 @@
+import { Component } from 'react';
+
 import classNames from 'classnames';
 
 import './employees-list-item.css';
 
-export const EmployeesListItem = ({ name, salary, increase }) => (
-  <li className={classNames('list-group-item d-flex justify-content-between', {'increase': increase})}>
-    <span className='list-group-item-label'>{name}</span>
-    <input
-      type='text'
-      className='list-group-item-input'
-      defaultValue={salary + '$'}
-    />
-    <div className='d-flex justify-content-center align-items-center'>
-      <button type='button' className='btn-cookie btn-sm '>
-        <i className='fas fa-cookie'></i>
-      </button>
+class EmployeesListItem extends Component { 
+  constructor(props) {
+    super(props);
+    this.state = {
+      increase: false,
+      rise: false
+    };
+  }
 
-      <button type='button' className='btn-trash btn-sm '>
-        <i className='fas fa-trash'></i>
-      </button>
-      <i className='fas fa-star'></i>
-    </div>
-  </li>
-);
+  isIncrease = () => {
+    this.setState(({increase}) => ({
+      increase: !increase
+    }));
+  };
+
+  isRise = () => {
+    this.setState(({rise}) => ({
+      rise: !rise
+    }));
+  };
+
+  render() {
+    const {name, salary} = this.props;
+    const {increase, rise} = this.state;
+  
+    return (
+      <li className={classNames(
+        'list-group-item d-flex justify-content-between', 
+          {
+            'increase': increase, 
+            'like': rise
+          }
+        )}
+      >
+        <span 
+          className='list-group-item-label'
+          onClick={this.isRise}
+        >{name}</span>
+        <input
+          type='text'
+          className='list-group-item-input'
+          defaultValue={salary + '$'}
+        />
+        <div className='d-flex justify-content-center align-items-center'>
+          <button 
+            type='button' 
+            className='btn-cookie btn-sm '
+            onClick={this.isIncrease}
+          >
+            <i className='fas fa-cookie'></i>
+          </button>
+
+          <button type='button' className='btn-trash btn-sm '>
+            <i className='fas fa-trash'></i>
+          </button>
+          <i className='fas fa-star'></i>
+        </div>
+      </li>
+    );
+  }
+}
+
+export default EmployeesListItem;
