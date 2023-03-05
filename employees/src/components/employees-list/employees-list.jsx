6b73566bf380926c1dc5227eb411af785d909524
Dmitry-Lipinskiy <Dimalipa@mail.ru>
2023-03-05 19:23:19 +0300

    passing data to app-info

--- employees/src/components/employees-list/employees-list.jsx
+++ employees/src/components/employees-list/employees-list.jsx
@@ -2,13 +2,22 @@ import { EmployeesListItem } from '../employees-list-item';
 
 import './employees-list.css';
 
-export const EmployeesList = ({ employeesData, onDelete }) => (
+export const EmployeesList = ({ 
+  employeesData, 
+  onDelete,
+  onToggleProp 
+  // onToggleIncrease, 
+  // onToggleRise 
+}) => (
   <ul className='app-list list-group'>
     {employeesData.map(({id, ...itemProps}) => (
       <EmployeesListItem 
         key={id} 
         {...itemProps} 
         onDelete={() => onDelete(id)}
+        onToggleProp={(e) => onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))}
+        // onToggleIncrease={() => onToggleIncrease(id)}
+        // onToggleRise={() => onToggleRise(id)}
       />
     ))}
   </ul>
