394747512ae7231055635469d8ee52fa4e3b53e3
Dmitry-Lipinskiy <Dimalipa@mail.ru>
2023-05-01 18:24:20 +0300

    add char search form, char page

--- marvel-2/src/components/app/App.js
+++ marvel-2/src/components/app/App.js
@@ -6,7 +6,8 @@ import { Spinner } from '../spinner/Spinner';
 
 const MainPage = lazy(() => import('../pages/mainPage/MainPage'));
 const ComicsPage = lazy(() => import('../pages/comicsPage/ComicsPage'));
-const SingleComicPage = lazy(() => import('../pages/singleComicPage/SingleComicPage'));
+const SingleComicPage = lazy(() => import('../pages/singlePage/SingleComicPage'));
+const SingleCharPage = lazy(() => import('../pages/singlePage/SingleCharPage'));
 const Page404 = lazy(() => import('../pages/page404/Page404'));
 
 export const App = () => (
@@ -17,6 +18,7 @@ export const App = () => (
         <Suspense fallback={<Spinner />}>
           <Routes>
             <Route path='/' element={<MainPage />} />
+            <Route path='/:charName' element={<SingleCharPage />} />
             <Route path='/comics' element={<ComicsPage />} />
             <Route path='/comics/:comicId' element={<SingleComicPage />} />
             <Route path='*' element={<Page404 />} /> 
